973a54727a993c1f764b0e3298586992aa0bc182
Nadia Coppens <copp7925@mylaurier.ca>
2025-01-15 23:19:16 -0500

    changing SimpleGreetingProgram

--- SimpleGreetingProgram.py
+++ SimpleGreetingProgram.py
@@ -1,5 +1,13 @@
 import datetime
+
+# ask for user name
 name = input('What is your name?')
+
+# ask for user age
 age = int(input('what is your age?'))
+
+# print greeting
 print('Nice to meet you',name)
+
+# calculate year user was born, then display it
 print(f'You were born in {datetime.datetime.now().year-age-1}')
