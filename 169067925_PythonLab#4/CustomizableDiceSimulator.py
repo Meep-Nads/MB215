64a9b04328b4e25a82f55fb23f213dfa28d8f2cd
Nadia Coppens <copp7925@mylaurier.ca>
2025-01-30 12:14:29 -0500

    Begin of lab#4

---
+++ 169067925_PythonLab#4/CustomizableDiceSimulator.py
@@ -0,0 +1,29 @@
+import random
+
+# Activity 1: Customizable Dice Simulator
+def roll_dice(num_dice, num_sides):
+    total = 0
+
+    #Loop for dice roll based on num of dice inputed
+    for num in range(num_dice):
+        a = 0
+
+        #determining dice roll based on num of sides inputed
+        a = random.randint(1, num_sides)
+
+        ###print(f'roll is: {a}')### (please ignore for debuging)
+        
+        #add dice roll to total 
+        total += a
+    return total
+
+#Input for how many dice to roll
+num_dice = int(input('How many dice would you like to roll: '))
+
+#Input for many sides each dice should have
+num_sides = int(input('Enter amount of dice sides: '))
+
+#print sum of all dice rolled 
+print(f'Rolling {num_dice} D{num_sides} ... Result is {roll_dice(num_dice, num_sides)}')
+
+ 
