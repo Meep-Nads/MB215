9271fa0525d95f8e244ef0590f587468993a121d
Nadia Coppens <copp7925@mylaurier.ca>
2025-03-12 15:26:58 -0400

    Assignment continue

--- 169067925_Assignment#1/DistanceBetweenTwoPoints.py
+++ 169067925_Assignment#1/DistanceBetweenTwoPoints.py
@@ -1,12 +1,54 @@
 #Develop a function in Python to determine if two spherical balls on a flat surface are in contact with each other.
-#The contact between two balls is established by calculating the distance between their centers. The coordinates (x1, y1) and (x2, y2) represent the centers of the first and second balls, respectively. To determine the distance, we apply the Pythagorean theorem as follows:
+
+#The contact between two balls is established by calculating the distance between their centers.
+#The coordinates (x1, y1) and (x2, y2) represent the centers of the first and second balls, respectively.
+#To determine the distance, we apply the Pythagorean theorem as follows:
 #distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 #When the sum of the radii of both balls is greater than or equal to this distance, it implies that the balls are touching.
+
 #Task Description:
-#1.	Input the x, y and radius of 2 balls
-#2.	Output the x, y and radius as they were input
-#3.	Output the distance between the center of the balls
-#4.	Output whether the balls are touching or not.
+import math
+#Input for corrdinates and radius calculation for Ball1 and Ball2
+#Input the y-corrdinate for ball 1
+ball1_xcor = int(input('Enter x-coordinate for ball 1:'))
+
+#Input the y-corrdinate for ball 1
+ball1_ycor = int(input('Enter y-coordinate for ball 1:'))
+
+#Input radius for ball 1
+ball1_radius = int(input('Enter radius for ball 1:'))
+
+#Input the x-corrdinate for ball 2
+ball2_xcor = int(input('Enter x-coordinate for ball 2:'))
+
+#Input the y-corrdinate for ball 2
+ball2_ycor = int(input('Enter y-coordinate for ball 2:'))
+
+#Input radius for ball 2
+ball2_radius = int(input('Enter radius for ball 2:'))
+
+#Output x, y and radius inputs for ball 1
+print(f'Ball 1 = Center: {(ball1_xcor,ball1_ycor)}, Radius: {ball1_radius}')
+
+#Output x, y and radius inputs for ball 2
+print(f'Ball 2 = Center: {(ball2_xcor,ball2_ycor)}, Radius: {ball2_radius}')
+
+#Calculate the distance between the center of the balls
+distance = math.sqrt((ball2_xcor - ball1_xcor)**2 + (ball2_ycor - ball1_ycor)**2)
+
+#Print the distance between the center of the balls
+print(f'Distance between centers: {distance}')
+
+#Output whether the balls are touching or not.
+if distance <= ball1_radius + ball2_radius:
+    print('Result: The balls are touching.')
+else:
+    print('Result: The balls are not touching.')
+
+
+
+
+
 #Expected Output:
 #Your program should display the following:
 #•	The x, y coordinates and radius for each ball as entered by the user.
---
+++ 169067925_Assignment#1/LettersBefore“m”InTheAlphabetAnd“m”OrAfter.py
@@ -0,0 +1,21 @@
+#Question 3. Letters before “m” in the alphabet and “m” or after
+
+#Create a Python program with two distinct functions to count occurrences of letters in a string based on their position in the alphabet relative to the letter 'm'.
+#Task Description:
+#1.	Function count_before_m:
+#o	This function will take a single string argument.
+#o	It will return the count of all letters within the string that come before 'm' in the alphabet.
+#2.	Function count_m_and_after:
+#o	This function will also take a single string argument.
+#o	It will return the count of all letters within the string that are 'm' or come after 'm' in the alphabet.
+#3.	User Input:
+#o	Prompt the user to enter a string.
+#o	Ensure that the string is evaluated case-insensitively, meaning 'M' and 'm' are treated equally.
+#4.	Output Display:
+#o	Using the input string, call both functions and display the results.
+#o	Show the user the count of letters before 'm' and the count of letters 'm' and after.
+#Sample output:
+#Please enter a string: Amazingly few discotheques provide jukeboxes.
+
+#Number of letters before 'm' in the alphabet: 24
+#Number of letters 'm' or after in the alphabet: 2
